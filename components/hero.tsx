c6760028bcc54fad378df8b4600333d4398704d4
Arthur Kurghinyan <a_kurghinyan@gohacktech.com>
2025-09-25 18:05:49 +0400

    hide arrow

--- components/hero.tsx
+++ components/hero.tsx
@@ -83,9 +83,9 @@ export function Hero() {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <ArrowDown className="w-6 h-6 text-muted-foreground" />
-      </div>
+      </div> */}
     </section>
   );
 }
